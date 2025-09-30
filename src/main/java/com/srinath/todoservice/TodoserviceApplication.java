490ed0a09d21917d3a42ec27c4e0efb96a1f4da1
Srinath S <KK@MacBookAir.fritz.box>
2025-09-30 20:23:48 +0200

    Initial commit with skeleton project generated from spring intializr

---
+++ src/main/java/com/srinath/todoservice/TodoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.srinath.todoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srinath/todoservice/TodoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srinath.todoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
